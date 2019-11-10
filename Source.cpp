838e586c8b05716255d7ed4685e611a87c03e7ae
MohamedAboushnief <mohamed.shenif@gmail.com>
2019-11-10 17:59:47 +0200

    finished shooting

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,11 @@
 #include <GL/glut.h>
 #include <ctime>
 
+#include <iostream>
+#include <string>
+
+bool repeat = false;
+float camera = 8.0;
 
 float rotAng;
 
@@ -14,51 +19,76 @@ float bally = 0;
 float ballz = 0;
 
 //for controlling ball direction before firing
-float directionXR = 0;
-float directionXL = 0;
+float directionX = 0;
+float directionY = 0;
 
-float directionYU = 0;
-float directionYD = 0;
-
-
-bool bottomWallCollision = false;
-bool rightWallCollision = false;
-bool leftWallCollision = false;
-bool topWallCollision = false;
-bool throwBall = false;
 
 bool right = false;
 bool left = false;
+bool up = false;
+bool down = false;
+
+bool verticalWallCollisionU = false;
+bool verticalWallCollisionD = false;
+bool sideWallCollisionR = false;
+bool sideWallCollisionL = false;
+bool throwBall = false;
+
+float angle = 0;  
+int arrowX = 0; 
+int arrowY = 0;
+
+bool disableKeyboard = false;
+
+int gameRounds = 3;
 
 
+
+void displayText(float x, float y,float z, int r, int g, int b, const char* string) {
+	int j = strlen(string);
+	glColor3f(r, g, b);
+	glRasterPos3f(x, y,z);
+	for (int i = 0; i < j; i++) {
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, string[i]);
+	}
+}
 
 void key(unsigned char k, int x, int y)
 
 {
-	
+	if (disableKeyboard == false) {
 
-	if (k == 'w') {		 
-		directionYU += 0.01;
-	}
-	if (k == 's') {
+		if (k == 'w') {
+			up = true;
+			if (directionY < 0.008) {
+				directionY += 0.0001;
+				//arrowY = 1;
+			}
+		}
+		if (k == 's') {
+			down = true;
+			if (directionY < 0.008) {
+				directionY -= 0.0001;
+				//arrowY = 1;
+			}
+		}
 
-		directionYD -= 0.01;
-	}
-	if (k == 'a') {
-		left = true;
-		if (directionXL > -0.008) {
-			directionXL -= 0.0001;
+		if (k == 'd') {
+			right = true;
+			if (directionX < 0.008) {
+				directionX += 0.0001;
+				//arrowX = 1;
+			}
+		}
+
+		if (k == 'a') {
+			left = true;
+			if (directionX > -0.008) {
+				directionX -= 0.0001;
+				//arrowX = 1;
+			}
 		}
 	}
-	if (k == 'd') {
-		right = true;
-		if (directionXR<0.008) {
-			directionXR += 0.0001;
-		}	
-	}
-	/*if (k == 'x') {
-
-	}*/
 	
 	glutPostRedisplay();
 }
@@ -69,6 +99,11 @@ void keyUp(unsigned char k, int x, int y)//keyboard up function is called whenev
 
 	if (k == 'x') {
 		throwBall = true;
+		disableKeyboard = true;
+	}
+
+	if (k == 'r') {
+		repeat = true;
 	}
 	glutPostRedisplay();
 }
@@ -290,8 +325,11 @@ void drawRoom() {
 void drawArrow() {
 	glPushMatrix();
 	glColor3f(1.0, 0.0, 0.0);
-	glScalef(0.1,0.1,10.0);
-	glTranslated(0, -5 ,0.1);
+	//glRotatef(-rotAng, 1, 1, 0);
+	glScalef(0.2,0.2,1);
+	glTranslated(0, 0, 4);
+	glRotatef(-20,arrowX,arrowY,0);
+	
 	glutSolidCube(0.5);
 	glPopMatrix();
 
@@ -299,9 +337,7 @@ void drawArrow() {
 
 void drawBall() {
 	glPushMatrix();
-	//glTranslatef(ballx, -1, ballz);
 	glTranslatef(ballx, bally, ballz);
-
 	glColor3f(1.0f, 0.0f, 1.0f);
 	glutSolidSphere(0.3, 25, 25);
 	glPopMatrix();
@@ -310,8 +346,31 @@ void drawBall() {
 
 void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0f, 0.0f, camera, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
 	drawBall();
+	//drawArrow();
 	drawRoom();
+	char result[100];   // array to hold the result.
+
+	if (gameRounds == 3) {
+		displayText(-1, -2.3, 1, 1, 0, 0, "Game Round: 3");
+	}
+	if (gameRounds == 2) {
+		displayText(-1, -2.3, 1, 1, 0, 0, "Game Round: 2");
+	}
+	if (gameRounds == 1) {
+		displayText(-1, -2.3, 1, 1, 0, 0, "Game Round: 1");
+	}
+	if (gameRounds == 0) {
+		displayText(-1, -2.3, 1, 1, 0, 0, "Game Over");
+	}
+
+
+
+
 	glFlush();
 }
 
@@ -319,74 +378,100 @@ void Anim() {
 	rotAng += 0.08;
 
 
+	if (repeat == true) {
 
+	}
+
+	
 	if (throwBall == true) {
-
-
-
-
+		//camera -= 0.002;
 
 		if (bally <= -2.5) {									// hit bottom wall
-			bottomWallCollision = true;
-
+			verticalWallCollisionD = true;
+			verticalWallCollisionU = false;
 		}
-		if (ballx >= 3) {										// hit right wall
-			rightWallCollision = true;
-		}
-
-		if (ballx <= -3) {										// hit left wall
-			leftWallCollision = true;
-		}
-
 		if (bally >= 3) {										// hit top wall
-			topWallCollision = true;
+			verticalWallCollisionU = true;
+			verticalWallCollisionD = false;
 		}
 
 
-		printf("%d\n",leftWallCollision);
+		if (ballx >= 3) {										// hit right wall
+			sideWallCollisionR = true;
+			sideWallCollisionL = false;
+		}
+		if (ballx <= -3) {										// hit left wall
+			sideWallCollisionR = false;
+			sideWallCollisionL = true;
+		}
+
+		if (ballz <= -15.5) {									// stick to the end wall
+			gameRounds -= 1;
+			ballx = 0;
+			bally = 0;
+			ballz = 0;
+			directionX = 0;
+			directionY = 0;
+
+
+			right = false;
+			left = false;
+			up = false;
+			down = false;
+
+			verticalWallCollisionU = false;
+			verticalWallCollisionD = false;
+			sideWallCollisionR = false;
+			sideWallCollisionL = false;
+			throwBall = false;
+
+			angle = 0;
+			arrowX = 0;
+			arrowY = 0;
+			//camera =8.0;
+
+			disableKeyboard = false;
+			
+		}
+
+
 
 		ballz -= 0.003;
 		
-		if (bottomWallCollision == true) {  
-			topWallCollision = false;
-			bally += 0.008;
-		}
-		else {
-			bally -= 0.008;
-		}
-	/*	if (right == true) {
-			if (rightWallCollision == true) {
-				leftWallCollision = false;
-				ballx -= directionXR;
+
+		if (up== true) {
+			if (verticalWallCollisionU == true) {
+				bally -= directionY;
 			}
 			else {
-				ballx += directionXR;
+				bally += directionY;
 			}
 		}
+		if (down == true) {
+			if (verticalWallCollisionD== true) {
+				bally -= directionY;
+			}
+			else {
+				bally += directionY;
+			}
+		}
+
 		if (left == true) {
-			if (leftWallCollision == true && left == true) {
-				rightWallCollision = false;
-				ballx += directionXL;
+			if (sideWallCollisionL == true) {
+				ballx -= directionX;
 			}
 			else {
-				ballx -= directionXL;
+				ballx += directionX;
 			}
-		}*/
-
-		
-
-
-
-		if (topWallCollision == true) {
-			bottomWallCollision = false;
-			bally -= 0.008;
 		}
-		else {
-			bally += 0.008;
+		if (right == true) {
+			if (sideWallCollisionR == true) {
+				ballx -= directionX;
+			}
+			else {
+				ballx += directionX;
+			}
 		}
-
-
-
 		
 
 	}
@@ -423,7 +508,7 @@ void main(int argc, char** argv) {
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0.0f, 0.0f, 8.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	//gluLookAt(0.0f, 0.0f, 8.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
 	glutMainLoop();
 }
